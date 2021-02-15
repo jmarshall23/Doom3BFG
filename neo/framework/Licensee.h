e2ff79065735e4efaaf8eb57d658ef0ce4f74878
Justin Marshall <justinmarshall20@gmail.com>
2021-02-14 23:31:06 -0800

    Updated branch name.

--- neo/framework/Licensee.h
+++ neo/framework/Licensee.h
@@ -44,7 +44,7 @@ If you have questions concerning this license or the applicable additional terms
 // RB end
 
 // jmarshall
-#define ENGINE_BRANCH					"Iced-Lab06-Dev"
+#define ENGINE_BRANCH					"Iced-Lab666-Dev"
 // jmarshall end
 
 #define	BASE_GAMEDIR					"base"
