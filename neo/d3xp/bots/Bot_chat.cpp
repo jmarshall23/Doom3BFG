817d11bf16b6fde0b7be839eefe9e9a7aab27018
Justin Marshall <justinmarshall20@gmail.com>
2021-02-24 20:24:36 -0800

    Added bot sender name at start of bot chat text.

--- neo/d3xp/bots/Bot_chat.cpp
+++ neo/d3xp/bots/Bot_chat.cpp
@@ -5,28 +5,28 @@
 #include "../Game_local.h"
 
 const char *bot_kill_insult[] = {
-	"Try aiming next time %s",
-	"Trust me when I say this %s, you suck.",
-	"Maybe you should go back to go cart racing %s?",
-	"Your technique reminds me of a story %s, a very dull one."
+	"%s: Try aiming next time %s",
+	"%s: Trust me when I say this %s, you suck.",
+	"%s: Maybe you should go back to go cart racing %s?",
+	"%s: Your technique reminds me of a story %s, a very dull one."
 };
 
 const char* bot_death_insult[] = {
-	"Mmmmmmm. Was it good for you too %s?",
-	"Well I guess %s won the lottery.",	
-	"My mother fragged me once %s. Once.",
-	"I'm gonna pull out your bowels %s",
-	"Beginners luck %s... Again",
-	"So %s... you're up to what ... 3 frags an hour?"
+	"%s: Mmmmmmm. Was it good for you too %s?",
+	"%s: Well I guess %s won the lottery.",	
+	"%s: My mother fragged me once %s. Once.",
+	"%s: I'm gonna pull out your bowels %s",
+	"%s: Beginners luck %s... Again",
+	"%s: So %s... you're up to what ... 3 frags an hour?"
 };
 
 const char* bot_death_praise[] = {
-	"%s not bad for an amateur.",
-	"%s alright that was pretty good. Your still a dousche.",
-	"That was definitely ... um ... pretty good %s",
-	"I've seen better %s, but not many.",
-	"Take a moment to reflect on your accomplishment %s",
-	"Your pretty good for a dousche %s"
+	"%s: %s not bad for an amateur.",
+	"%s: %s alright that was pretty good. Your still a dousche.",
+	"%s: That was definitely ... um ... pretty good %s",
+	"%s: I've seen better %s, but not many.",
+	"%s: Take a moment to reflect on your accomplishment %s",
+	"%s: Your pretty good for a dousche %s"
 };
 
 /*
@@ -38,16 +38,16 @@ void rvmBot::BotSendChatMessage(botChat_t chat, const char* targetName) {
 	switch (chat)
 	{
 		case KILL:
-			gameLocal.mpGame.AddChatLine(bot_kill_insult[rvRandom::irand(0, 3)], targetName);
+			gameLocal.mpGame.AddChatLine(bot_kill_insult[rvRandom::irand(0, 3)], netname.c_str(), targetName);
 			break;
 		case DEATH:
 			if (rvRandom::irand(0, 10) < 5)
 			{
-				gameLocal.mpGame.AddChatLine(bot_death_insult[rvRandom::irand(0, 5)], targetName);
+				gameLocal.mpGame.AddChatLine(bot_death_insult[rvRandom::irand(0, 5)], netname.c_str(), targetName);
 			}
 			else
 			{
-				gameLocal.mpGame.AddChatLine(bot_death_praise[rvRandom::irand(0, 5)], targetName);
+				gameLocal.mpGame.AddChatLine(bot_death_praise[rvRandom::irand(0, 5)], netname.c_str(), targetName);
 			}
 			break;
 	}
